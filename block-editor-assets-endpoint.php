3c603a16b5b97afc9ac6eed5e975c4d5df09a830
David Calhoun <github@davidcalhoun.me>
2024-07-31 12:10:08 -0400

    fix: Allow REST API requests from the editor
    Fix CORS errors due to a disallowed `X-Wp-Api-Fetch-From-Editor` header.

--- block-editor-assets-endpoint.php
+++ block-editor-assets-endpoint.php
@@ -130,5 +130,11 @@ add_action("rest_api_init", function () {
 function create_block_custom_block_block_init() {
 	register_block_type( __DIR__ . '/custom-block/build' );
 }
-
 add_action( 'init', 'create_block_custom_block_block_init' );
+
+/**
+ * Allow the REST API to accept requests from the editor.
+ */
+add_action( 'rest_pre_serve_request', function () {
+		header('Access-Control-Allow-Headers: X-Wp-Api-Fetch-From-Editor, X-Wp-Nonce, Content-Type');
+} );
