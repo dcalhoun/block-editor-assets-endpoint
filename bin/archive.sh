986a1ff905e6adc55972f9a952130a65922c203a
David Calhoun <github@davidcalhoun.me>
2024-07-31 15:24:09 -0400

    feat: Improve archive script

--- bin/archive.sh
+++ bin/archive.sh
@@ -2,7 +2,7 @@
 
 # Define the source directory and ZIP file name
 SOURCE_DIR="$(dirname "$(dirname "$0")")"
-ZIP_FILE="archive.zip"
+ZIP_FILE="block-editor-assets-endpoint.zip"
 
 # Create a temporary directory to store the files to be archived
 TEMP_DIR=$(mktemp -d)
@@ -10,22 +10,24 @@ TEMP_DIR=$(mktemp -d)
 # Copy all files and directories from the source directory to the temporary directory,
 # excluding the node_modules directory and .gitignore file
 rsync -av \
+  --exclude='.DS_Store' \
   --exclude='node_modules' \
   --exclude='.gitignore' \
   --exclude='.git' \
   --exclude='bin' \
   --exclude='**/.editorconfig' \
   --exclude='**/package-lock.json' \
-  "$SOURCE_DIR/" "$TEMP_DIR/"
+  "$SOURCE_DIR/" "$TEMP_DIR/" \
+  > /dev/null
 
 # Change to the temporary directory
 pushd "$TEMP_DIR" > /dev/null
 
 # Create the ZIP file
-zip -r "$ZIP_FILE" .
+zip -r "$TEMP_DIR/$ZIP_FILE" ./* > /dev/null
 
 # Change back to the original directory
-popd
+popd > /dev/null
 
 # Move the ZIP file to the desired location
 mv "$TEMP_DIR/$ZIP_FILE" "$SOURCE_DIR"
